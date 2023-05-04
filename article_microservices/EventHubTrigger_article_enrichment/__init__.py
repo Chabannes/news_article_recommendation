7d6b22fcc84752ffdb2abfe4b8af3b60121f0b81
Francois <chabannes.francois@gmail.com>
2023-05-04 15:49:04 +0100

    test dummy functions

--- article_microservices/EventHubTrigger_article_enrichment/__init__.py
+++ article_microservices/EventHubTrigger_article_enrichment/__init__.py
@@ -15,213 +15,217 @@ from gremlin_python.driver.driver_remote_connection import DriverRemoteConnectio
 from sentence_transformers import SentenceTransformer, util
 
 
-import nest_asyncio
-nest_asyncio.apply()
-import asyncio
+# import nest_asyncio
+# nest_asyncio.apply()
+# import asyncio
 
 
-async def get_best_tags(properties):
-    tags = ['sport', 'tech', 'politics', 'entertainment', 'business']
-    best_tags = []
-    for tag in tags:
-        if properties.get(tag) and properties[tag] <= 2:
-            best_tags.append(tag)
-    return best_tags
+# async def get_best_tags(properties):
+#     tags = ['sport', 'tech', 'politics', 'entertainment', 'business']
+#     best_tags = []
+#     for tag in tags:
+#         if properties.get(tag) and properties[tag] <= 2:
+#             best_tags.append(tag)
+#     return best_tags
 
 
 
-async def get_labels(text):
+# async def get_labels(text):
 
 
-    headers = {
-        'Authorization': 'Bearer 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',
-        'Content-Type': 'application/json'
-    }
+#     headers = {
+#         'Authorization': 'Bearer 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',
+#         'Content-Type': 'application/json'
+#     }
 
-    data = '{"inputs": "%s"}' % text[:512].replace('"', '\\"').replace("'", "\\'").encode('utf-8', 'replace')
-    url = 'https://api-inference.huggingface.co/models/abhishek/autonlp-bbc-news-classification-37229289'
+#     data = '{"inputs": "%s"}' % text[:512].replace('"', '\\"').replace("'", "\\'").encode('utf-8', 'replace')
+#     url = 'https://api-inference.huggingface.co/models/abhishek/autonlp-bbc-news-classification-37229289'
 
-    logging.info('TEXT %s', text)
+#     logging.info('TEXT %s', text)
 
-    response = requests.post(url, headers=headers, data=data)
+#     response = requests.post(url, headers=headers, data=data)
 
-    logging.info('**RESPONSE: %s', response.text)
+#     logging.info('**RESPONSE: %s', response.text)
 
-    t = 0
-    while True and t < 10:
+#     t = 0
+#     while True and t < 10:
 
-        response = requests.post(url, headers=headers, data=data)
+#         response = requests.post(url, headers=headers, data=data)
         
-        if response.ok:
-            # Extract the list of labels and scores from the predictions
-            label_scores = response.json()[0]
+#         if response.ok:
+#             # Extract the list of labels and scores from the predictions
+#             label_scores = response.json()[0]
             
-            # Sort the labels based on the scores
-            sorted_labels = sorted(label_scores, key=lambda x: x['score'], reverse=True)
+#             # Sort the labels based on the scores
+#             sorted_labels = sorted(label_scores, key=lambda x: x['score'], reverse=True)
             
-            # Create a dictionary with the ranking of each label
-            rankings = {}
-            for i, label in enumerate(sorted_labels):
-                rankings[label['label']] = i + 1
+#             # Create a dictionary with the ranking of each label
+#             rankings = {}
+#             for i, label in enumerate(sorted_labels):
+#                 rankings[label['label']] = i + 1
 
-            logging.info('**RANKINGS: %s', rankings)
+#             logging.info('**RANKINGS: %s', rankings)
 
-            return rankings
+#             return rankings
         
-        elif "currently loading" in response.text:
-            estimated_time = response.json()["estimated_time"]
-            logging.info("Model is currently loading, waiting for %.2f seconds before retrying", 8)
-            await asyncio.sleep(8)            
-            t = t + 1
-        else:
-            raise ValueError(f"Failed to get response from Hugging Face API: {response.text}")
+#         elif "currently loading" in response.text:
+#             estimated_time = response.json()["estimated_time"]
+#             logging.info("Model is currently loading, waiting for %.2f seconds before retrying", 8)
+#             await asyncio.sleep(8)            
+#             t = t + 1
+#         else:
+#             raise ValueError(f"Failed to get response from Hugging Face API: {response.text}")
 
 
-    logging.info('**Error in model-tagging API call: %s', response.text)
-    raise ValueError('Could not get answer model-tagging API: %s', response.text)
+#     logging.info('**Error in model-tagging API call: %s', response.text)
+#     raise ValueError('Could not get answer model-tagging API: %s', response.text)
 
 
 async def main(articles: func.EventHubEvent):
 
-    new_articles = articles
+    logging.info('**INFO: HELLO WORLD')
 
-    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-    sim_threshold = 0.3
+    # new_articles = articles
 
-    logging.info('**INFO: number of arriving articles: %s', len(new_articles))
+    # model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+    # sim_threshold = 0.3
+
+    # logging.info('**INFO: number of arriving articles: %s', len(new_articles))
     
-    gremlin_client = None
+    # gremlin_client = None
 
-    cosmosDB_endpoint = os.environ['cosmosDB_endpoint']
-    cosmos_database_name = os.environ['cosmos_database_name']
-    graph_name = os.environ['graph_name']
-    cosmosDB_key = os.environ['cosmosDB_key']
+    # cosmosDB_endpoint = os.environ['cosmosDB_endpoint']
+    # cosmos_database_name = os.environ['cosmos_database_name']
+    # graph_name = os.environ['graph_name']
+    # cosmosDB_key = os.environ['cosmosDB_key']
 
-    logging.info('cosmosDB_endpoint: %s', cosmosDB_endpoint)
-    logging.info('cosmos_database_name: %s', cosmos_database_name)
-    logging.info('graph_name: %s', graph_name)
-    logging.info('cosmosDB_key: %s', cosmosDB_key)
-
-
-    # Create a Gremlin client and connect to the Cosmos DB graph
-    gremlin_client = client.Client(cosmosDB_endpoint, 'g',
-                                        username="/dbs/%s/colls/%s" % (cosmos_database_name, graph_name),
-                                        password=cosmosDB_key,
-                                        message_serializer=serializer.GraphSONSerializersV2d0()
-                                        )
+    # logging.info('cosmosDB_endpoint: %s', cosmosDB_endpoint)
+    # logging.info('cosmos_database_name: %s', cosmos_database_name)
+    # logging.info('graph_name: %s', graph_name)
+    # logging.info('cosmosDB_key: %s', cosmosDB_key)
 
 
-    for new_article in new_articles:
-        logging.info('Python EventHub trigger processed an event: %s', new_article.get_body().decode('utf-8'))
-        new_article_dict = json.loads(new_article.get_body().decode('utf-8'))
+    # # Create a Gremlin client and connect to the Cosmos DB graph
+    # gremlin_client = client.Client(cosmosDB_endpoint, 'g',
+    #                                     username="/dbs/%s/colls/%s" % (cosmos_database_name, graph_name),
+    #                                     password=cosmosDB_key,
+    #                                     message_serializer=serializer.GraphSONSerializersV2d0()
+    #                                     )
 
-        # Check if article is already in DB
-        # Define the Gremlin query to check if there is an existing article with similar title and source
-        check_query_already_exist = """
-                g.V().has('article', 'title', title).has('source', source)
-        """
 
-        # Execute the Gremlin query with the given article properties to check if an article already exists
-        result_set = gremlin_client.submit(check_query_already_exist, {
-                'title': new_article_dict['title'],
-                'source': new_article_dict['source']
-                            })
+    # for new_article in new_articles:
+    #     logging.info('Python EventHub trigger processed an event: %s', new_article.get_body().decode('utf-8'))
+    #     new_article_dict = json.loads(new_article.get_body().decode('utf-8'))
 
-        # If there are no existing articles, add the new article vertex
-        if not result_set.all().result():
-            # Define the Gremlin query to insert the article vertex
+    #     # Check if article is already in DB
+    #     # Define the Gremlin query to check if there is an existing article with similar title and source
+    #     check_query_already_exist = """
+    #             g.V().has('article', 'title', title).has('source', source)
+    #     """
 
-            # Create a dictionary of properties for the new article
-            properties = {
-                    'title': new_article_dict['title'],
-                    'content': new_article_dict['content'],
-                    'source': new_article_dict['source'],
-                    'publishedAt': new_article_dict['publishedAt'],
-                    'partitionKey': new_article_dict['publishedAt']
-            }
+    #     # Execute the Gremlin query with the given article properties to check if an article already exists
+    #     result_set = gremlin_client.submit(check_query_already_exist, {
+    #             'title': new_article_dict['title'],
+    #             'source': new_article_dict['source']
+    #                         })
+
+    #     # If there are no existing articles, add the new article vertex
+    #     if not result_set.all().result():
+    #         # Define the Gremlin query to insert the article vertex
+
+    #         # Create a dictionary of properties for the new article
+    #         properties = {
+    #                 'title': new_article_dict['title'],
+    #                 'content': new_article_dict['content'],
+    #                 'source': new_article_dict['source'],
+    #                 'publishedAt': new_article_dict['publishedAt'],
+    #                 'partitionKey': new_article_dict['publishedAt']
+    #         }
                 
 
-            # Add properties for each tag and its corresponding score
-            label_scores = await get_labels(new_article_dict['title'])
+    #         # Add properties for each tag and its corresponding score
+    #         label_scores = await get_labels(new_article_dict['title'])
 
-            for tag, score in label_scores.items():
-                properties[tag] = score
+    #         for tag, score in label_scores.items():
+    #             properties[tag] = score
 
 
-            # get the best tags of the new article
-            best_tags = await get_best_tags(properties)
+    #         # get the best tags of the new article
+    #         best_tags = await get_best_tags(properties)
 
-            # Construct the query to select articles with the specified properties and label
-            query = "g.V().hasLabel('article')"
-            for tag in best_tags:
-                query += ".has('{}', lte(2))".format(tag)
-            query += ".limit(200).values('title').fold()"
+    #         # Construct the query to select articles with the specified properties and label
+    #         query = "g.V().hasLabel('article')"
+    #         for tag in best_tags:
+    #             query += ".has('{}', lte(2))".format(tag)
+    #         query += ".limit(200).values('title').fold()"
 
-            # Get the same-category articles
-            all_titles = gremlin_client.submit(query)
-            all_titles = all_titles.all().result()
-            all_titles = list(all_titles[0])
+    #         # Get the same-category articles
+    #         all_titles = gremlin_client.submit(query)
+    #         all_titles = all_titles.all().result()
+    #         all_titles = list(all_titles[0])
 
             
-            # Construct the Gremlin query with the properties
-            add_query = """
-                    g.addV('article')
-                        .property('title', title)
-                        .property('content', content)
-                        .property('source', source)
-                        .property('publishedAt', publishedAt)
-                        .property('partitionKey', partitionKey)
-                    """
+    #         # Construct the Gremlin query with the properties
+    #         add_query = """
+    #                 g.addV('article')
+    #                     .property('title', title)
+    #                     .property('content', content)
+    #                     .property('source', source)
+    #                     .property('publishedAt', publishedAt)
+    #                     .property('partitionKey', partitionKey)
+    #                 """
 
-            # Add properties for each tag and its corresponding score to the query
-            for tag, score in label_scores.items():
-                add_query += f"\n.property('{tag}', {score})"
+    #         # Add properties for each tag and its corresponding score to the query
+    #         for tag, score in label_scores.items():
+    #             add_query += f"\n.property('{tag}', {score})"
 
-            # Execute the Gremlin query with the given article properties
-            result_set = gremlin_client.submitAsync(add_query, properties)
-            logging.info('**INFO: Article successfully inserted')
+    #         # Execute the Gremlin query with the given article properties
+    #         result_set = gremlin_client.submitAsync(add_query, properties)
+    #         logging.info('**INFO: Article successfully inserted')
 
 
-            # Loop over all titles in the list and compute similarity
-            for title in all_titles:
+    #         # Loop over all titles in the list and compute similarity
+    #         for title in all_titles:
 
-                relationship_query = """
-                        g.V().has('article', 'title', title1).as('a')
-                            .V().has('article', 'title', title2).as('b')
-                            .coalesce(
-                                select('a').outE('similarity').where(inV().as('b')),
-                                addE('similarity').from('a').to('b').property('value', sim)
-                            )
-                                        """
+    #             relationship_query = """
+    #                     g.V().has('article', 'title', title1).as('a')
+    #                         .V().has('article', 'title', title2).as('b')
+    #                         .coalesce(
+    #                             select('a').outE('similarity').where(inV().as('b')),
+    #                             addE('similarity').from('a').to('b').property('value', sim)
+    #                         )
+    #                                     """
 
-                # Query the content property of the article with the matching title
+    #             # Query the content property of the article with the matching title
 
-                query = "g.V().hasLabel('article').has('title', title).values('content').fold()"
-                content = gremlin_client.submit(query, {'title': title}).all().result()
-                content = list(content[0])
+    #             query = "g.V().hasLabel('article').has('title', title).values('content').fold()"
+    #             content = gremlin_client.submit(query, {'title': title}).all().result()
+    #             content = list(content[0])
 
-                embedding_new_article = model.encode(new_article_dict['content'], convert_to_tensor=True)
-                embedding_article = model.encode(content, convert_to_tensor=True)
-                sim = util.pytorch_cos_sim(embedding_new_article, embedding_article)[0][0].item()
+    #             embedding_new_article = model.encode(new_article_dict['content'], convert_to_tensor=True)
+    #             embedding_article = model.encode(content, convert_to_tensor=True)
+    #             sim = util.pytorch_cos_sim(embedding_new_article, embedding_article)[0][0].item()
 
-                # If the similarity score is above the threshold, create a relationship between the articles
-                if sim >= sim_threshold:
-                    # Create a relationship between the new article and the current article
-                    result_set = gremlin_client.submitAsync(relationship_query, {
-                                'title1': new_article_dict['title'],
-                                'title2': title,
-                                'sim': sim
-                        })
+    #             # If the similarity score is above the threshold, create a relationship between the articles
+    #             if sim >= sim_threshold:
+    #                 # Create a relationship between the new article and the current article
+    #                 result_set = gremlin_client.submitAsync(relationship_query, {
+    #                             'title1': new_article_dict['title'],
+    #                             'title2': title,
+    #                             'sim': sim
+    #                     })
 
-                    logging.info('**INFO: similarity = %s', round(sim, 2))
+    #                 logging.info('**INFO: similarity = %s', round(sim, 2))
 
-        else:
-            logging.info('**INFO: Article is already in Database')
+    #     else:
+    #         logging.info('**INFO: Article is already in Database')
 
-    # Close the Gremlin client connection
-    gremlin_client.close()
+    # # Close the Gremlin client connection
+    # gremlin_client.close()
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+
+    main()
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
--- article_microservices/TimerTrigger_article_generation/__init__.py
+++ article_microservices/TimerTrigger_article_generation/__init__.py
@@ -13,138 +13,138 @@ import datetime
 import re
 
 
-logging.info('NEEEEEEEEEEEEEW')
 
-
-def remove_in_between(text, start_str, end_str):
+# def remove_in_between(text, start_str, end_str):
     
-    start_index = text.find(start_str)
-    end_index = text.find(end_str, start_index)
+#     start_index = text.find(start_str)
+#     end_index = text.find(end_str, start_index)
     
-    removed_substring = ''
-    if start_index != -1 and end_index != -1:
-        removed_substring = text[start_index:end_index + len(end_str)]
-        text = text[:start_index] + text[end_index + len(end_str):]
+#     removed_substring = ''
+#     if start_index != -1 and end_index != -1:
+#         removed_substring = text[start_index:end_index + len(end_str)]
+#         text = text[:start_index] + text[end_index + len(end_str):]
     
-    return text
+#     return text
 
 
-def add_space_after_dot(text):
-    return re.sub(r'\.(?! )', r'. ', text)
+# def add_space_after_dot(text):
+#     return re.sub(r'\.(?! )', r'. ', text)
 
 
-def clean_end_of_article(text):
+# def clean_end_of_article(text):
 
-    text = text.split("You may also be interested in")[0]
-    text = text.split("Related Topics")[0]
-    text = text.split("Read more here")[0]
-    text = text.split("Send your story ideas to")[0]
+#     text = text.split("You may also be interested in")[0]
+#     text = text.split("Related Topics")[0]
+#     text = text.split("Read more here")[0]
+#     text = text.split("Send your story ideas to")[0]
 
-    return text
+#     return text
 
 
-def find_longest_substring(text, start_str, end_str_list):
-    longest_substring = ''
-    longest_substring_len = 0
-    longest_end_str = None
+# def find_longest_substring(text, start_str, end_str_list):
+#     longest_substring = ''
+#     longest_substring_len = 0
+#     longest_end_str = None
 
-    for end_str in end_str_list:
-        start_index = text.find(start_str)
-        end_index = text.find(end_str, start_index)
+#     for end_str in end_str_list:
+#         start_index = text.find(start_str)
+#         end_index = text.find(end_str, start_index)
 
-        if start_index != -1 and end_index != -1:
-            substring = text[start_index+len(start_str):end_index]
-            if len(substring) > longest_substring_len and len(substring) < 200:
-                longest_substring = substring
-                longest_substring_len = len(substring)
-                longest_end_str = end_str
+#         if start_index != -1 and end_index != -1:
+#             substring = text[start_index+len(start_str):end_index]
+#             if len(substring) > longest_substring_len and len(substring) < 200:
+#                 longest_substring = substring
+#                 longest_substring_len = len(substring)
+#                 longest_end_str = end_str
     
-    return longest_end_str
+#     return longest_end_str
 
 
-def clean_start_of_article(text):
+# def clean_start_of_article(text):
 
-    try:
-        longest_end_str = find_longest_substring(text, start_str='Published', end_str_list=['Media caption', 'Image caption', 'Copy link', 'NurPhoto', 'Getty Images'])
-        text = remove_in_between(text, start_str='Published', end_str=longest_end_str)
+#     try:
+#         longest_end_str = find_longest_substring(text, start_str='Published', end_str_list=['Media caption', 'Image caption', 'Copy link', 'NurPhoto', 'Getty Images'])
+#         text = remove_in_between(text, start_str='Published', end_str=longest_end_str)
 
-    except:
-        pass
+#     except:
+#         pass
 
-    text = text.replace('Media caption, ', '')
-    return text
-
-
-def clean_middle_of_article(text):
-
-    text = remove_in_between(text, start_str='Last updated on', end_str='.')
-    text = remove_in_between(text, start_str='Available to UK users', end_str='sharingRead description')
-    text = text.replace('There was an errorThis content is not available in your location', '')
-    text = text.replace('To use comments you will need to have JavaScript enabled.', '')
-    text = text.replace('\n"', " ")
-    text = text.replace('\"', "' ")
-    text = text.replace('\u2026', ' ')
-
-    return text
+#     text = text.replace('Media caption, ', '')
+#     return text
 
 
-def clean_content(text):
+# def clean_middle_of_article(text):
 
-    logging.info("**RAW TEXT:")
-    logging.info(text)
+#     text = remove_in_between(text, start_str='Last updated on', end_str='.')
+#     text = remove_in_between(text, start_str='Available to UK users', end_str='sharingRead description')
+#     text = text.replace('There was an errorThis content is not available in your location', '')
+#     text = text.replace('To use comments you will need to have JavaScript enabled.', '')
+#     text = text.replace('\n"', " ")
+#     text = text.replace('\"', "' ")
+#     text = text.replace('\u2026', ' ')
 
-    text = clean_end_of_article(text)
-    text = clean_start_of_article(text)
-    text = clean_middle_of_article(text)
-    text = add_space_after_dot(text)
+#     return text
 
-    logging.info("**PROCESSED TEXT:")
-    logging.info(text)
 
-    return text
+# def clean_content(text):
+
+#     logging.info("**RAW TEXT:")
+#     logging.info(text)
+
+#     text = clean_end_of_article(text)
+#     text = clean_start_of_article(text)
+#     text = clean_middle_of_article(text)
+#     text = add_space_after_dot(text)
+
+#     logging.info("**PROCESSED TEXT:")
+#     logging.info(text)
+
+#     return text
 
     
 
 def main(timer: func.TimerRequest, outputMessage: func.Out[str]) -> None: 
 
-    utc_timestamp = datetime.datetime.utcnow().replace(
-            tzinfo=datetime.timezone.utc).isoformat()
+    logging.info('HELLO WORLD')
 
-    newsapi_key = "6fdb9fb9f5154d1c8073d732a744fb9f" 
-    newsapi = NewsApiClient(api_key=newsapi_key) 
+#     utc_timestamp = datetime.datetime.utcnow().replace(
+#             tzinfo=datetime.timezone.utc).isoformat()
+
+#     newsapi_key = "6fdb9fb9f5154d1c8073d732a744fb9f" 
+#     newsapi = NewsApiClient(api_key=newsapi_key) 
 
 
-    # Search for articles using the everything endpoint
-    article_list = []
-    articles = newsapi.get_everything(sources='bbc-news')
+#     # Search for articles using the everything endpoint
+#     article_list = []
+#     articles = newsapi.get_everything(sources='bbc-news')
 
-    # Retrieve the full content of each article using the urlToImage field
-    for article in articles['articles']:
-        # Make sure the article has a URL
-        if 'urlToImage' in article:
-            # Use the URL to fetch the full article content
-            response = requests.get(article['url'])
-            soup = BeautifulSoup(response.content, 'html.parser')
-            try:
+#     # Retrieve the full content of each article using the urlToImage field
+#     for article in articles['articles']:
+#         # Make sure the article has a URL
+#         if 'urlToImage' in article:
+#             # Use the URL to fetch the full article content
+#             response = requests.get(article['url'])
+#             soup = BeautifulSoup(response.content, 'html.parser')
+#             try:
 
-                # get content
-                article['content'] = soup.find('article').get_text()
+#                 # get content
+#                 article['content'] = soup.find('article').get_text()
 
-                # clean content
-                article['content'] = clean_content(article['content'])
+#                 # clean content
+#                 article['content'] = clean_content(article['content'])
 
-                article['source'] = article['source']['id']
-                article = {key: value for key, value in article.items() if key in ['source', 'title', 'publishedAt', 'content']}
+#                 article['source'] = article['source']['id']
+#                 article = {key: value for key, value in article.items() if key in ['source', 'title', 'publishedAt', 'content']}
 
-                if len(article['content']) > 800:
-                    logging.info("**INFO : new article : %s", article['title'])
-                    article_list.append(article)
+#                 if len(article['content']) > 800:
+#                     logging.info("**INFO : new article : %s", article['title'])
+#                     article_list.append(article)
 
-            except Exception as e:
-                logging.info("**INFO EXCEPTION :  : %s", e)
-                pass
+#             except Exception as e:
+#                 logging.info("**INFO EXCEPTION :  : %s", e)
+#                 pass
     
-    body = json.dumps(article_list)
-    outputMessage.set(body)
+#     body = json.dumps(article_list)
+#     outputMessage.set(body)
 
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+    # logging.info('Python timer trigger function ran at %s', utc_timestamp)
